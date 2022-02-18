64e885d61f1f804343020bd72153dfdf09c82688
Pratik-Durade <98595200+Pratik-Durade@users.noreply.github.com>
2022-02-18 18:33:25 +0530

    JDBC Annotation without using xml file

---
+++ JDBC Annotation/App.java
@@ -0,0 +1,17 @@
+package com.pratik;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		SpringJdbcSelect s=ac.getBean(SpringJdbcSelect.class);
+		
+		s.insert();
+		s.selectEx();
+	}
+
+}
