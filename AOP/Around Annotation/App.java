d2a0ab663399f1ee0deb06fc954a2f9774099e4c
Pratik-Durade <98595200+Pratik-Durade@users.noreply.github.com>
2022-02-23 00:07:28 +0530

    Around Advice Annotation Example

---
+++ AOP/Around Annotation/App.java
@@ -0,0 +1,15 @@
+package com.pratik.around;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Student s=ac.getBean(Student.class);
+		
+		s.demo();
+		s.show();
+	}
+}
---
+++ AOP/Around Annotation/Student.java
@@ -0,0 +1,15 @@
+package com.pratik.around;
+
+public class Student {
+	
+	void demo()
+	{
+		System.out.println("Demo method of Student class");
+	}
+	
+	void show()
+	{
+		System.out.println("Student Details :\n Name : Pratik Durade\n Address : Nipani " );
+	}
+
+}
